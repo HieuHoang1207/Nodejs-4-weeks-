be4f116c431dc651da49dbf88d356a9452b71e22
Hieu.mh7 <hieu.mh7@gmail.com>
2024-10-09 15:07:37 +0700

    lam bai 23

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const setupSwagger = require("./config/swagger");
 const userRoutes = require("./routes/userRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const homeRoutes = require("./routes/homeRoutes");
+const i18n = require("i18n");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,6 +31,29 @@ app.use(cors());
 // );
 app.use(helmet({}));
 app.use(bodyParser.json());
+
+// Cấu hình i18n
+i18n.configure({
+  locales: ["en", "vi"], // Các ngôn ngữ hỗ trợ
+  directory: __dirname + "/locales", // Đường dẫn đến thư mục chứa file ngôn ngữ
+  defaultLocale: "en", // Ngôn ngữ mặc định
+  queryParameter: "lang", // Nếu muốn lấy ngôn ngữ từ query string (không bắt buộc)
+  header: "accept-language", // Lấy ngôn ngữ từ header (cũng có thể dùng trường tùy chỉnh)
+  autoReload: true, // Tự động tải lại file ngôn ngữ nếu có thay đổi
+  updateFiles: false, // Không tự động ghi lại file ngôn ngữ khi thiếu key
+  objectNotation: true, // Hỗ trợ truy cập lồng nhau trong file JSON
+});
+
+// Sử dụng middleware i18n
+app.use(i18n.init);
+
+// Route ví dụ để kiểm tra ngôn ngữ
+app.get("/welcome", (req, res) => {
+  res.json({
+    message: res.__("welcome_message"),
+  });
+});
+
 app.use("/api/auth", authRoutes);
 
 app.use("/users", userRoutes);
